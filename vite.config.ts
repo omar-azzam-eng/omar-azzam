22a3b5bd9b91e4395fb51524de46b5c8f75d7d77
mehdiazzam <mehdiazzam81@gmail.com>
2026-01-17 11:17:24 +0300

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: '/omar-azzam-dev/',
+base: '/omar-azzam-dev',
   server: {
     host: "::",
     port: 8080,
